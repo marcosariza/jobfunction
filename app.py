40b91f6a01e894e95d3a57a6402a7d72255387a6
marcosariza <38824528+marcosariza@users.noreply.github.com>
2025-05-03 22:29:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     target = os.environ.get('TARGET', 'World')
-    return 'Hello 3 {}!\n'.format(target)
+    print('Se ha realizado una llamada')
+    return 'Hello {}!\n'.format(target)
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
